d523de15aea1b675d1079e5172a42483f9cc237a
andresengineer <115186337+andresengineer@users.noreply.github.com>
2023-11-29 00:44:21 -0500

    Puntos (3.1/3.2/3.3/3.4) Versión 1

---
+++ ProyectoFinal/ProyectoFinal/src/main/scala/ReconstCadenasPar/package.scala
@@ -0,0 +1,15 @@
+/*
+Proyecto Final:
+
+Wilson Andrés Mosquera Zapata <202182116>
+ <202124366>
+
+28/11/2023
+
+Archivo: package.scala (ReconstCadenasPar)
+
+*/
+
+package object ReconstCadenasPar {
+
+}
